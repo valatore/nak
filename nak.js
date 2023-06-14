58efea1b983ac3385df5fd39f15c92e2013178ff
Connor V <connorskittles@gmail.com>
2023-06-14 23:01:09 +0100

    Updated nak.js and added index.html

--- nak.js
+++ nak.js
@@ -2,6 +2,13 @@ const vay = 'Vay';
 const age = 29;
 
 console.log(`hello ${vay}. You are ${age}`);
+console.log(`hello ${vay}. You are ${age}`);
+console.log(`hello ${vay}. You are ${age}`);
+console.log(`hello ${vay}. You are ${age}`);
+console.log(`hello ${vay}. You are ${age}`);
+console.log(`hello ${vay}. You are ${age}`);
+console.log(`hello ${vay}. You are ${age}`);
+console.log(`hello ${vay}. You are ${age}`);
 
 function hello() {
   console.log('hi');
